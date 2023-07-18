6317bf19e7fb6761b4b38ab6e1d0bedef5f8232e
Sergey527 <jr.sergey.pervushin@gmail.com>
2023-07-18 10:48:16 +0300

    added full page

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,17 @@
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".promo__swiper", {
   direction: "horizontal",
   loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const swiper_products = new Swiper(".products-section__swiper", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 30,
 
   pagination: {
     el: ".swiper-pagination",
